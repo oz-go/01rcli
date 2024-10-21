843e1e4bcdde826f4f714d5a19a9ec61f5af8090
JxtH <914151610@qq.com>
2024-10-21 15:33:35 +0800

    feature: add password generate_pass.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // rcli csv -i input.csv -o output.json --header -d ','
 use clap::Parser;
-use rcli::{process_csv,Opts,SubCommand};
+use rcli::{process_csv, process_genpass, Opts, SubCommand};
 
 
 
@@ -20,6 +20,15 @@ fn main() -> anyhow::Result<()> {
                 format!("output.{}",opts.format)
             };
             process_csv(&opts.input, output,opts.format)?
+        }
+        SubCommand::GenPass(opts)=>{
+            // println!("Generate password:{:?}",opts);
+            process_genpass(
+                opts.length, 
+                opts.uppercase, 
+                opts.lowercase, 
+                opts.number, 
+                opts.symbol)?;
         }
     }
     // println!("{:?}",opts);
--- src/opts.rs
+++ src/opts.rs
@@ -12,6 +12,9 @@ pub struct Opts{
 pub enum SubCommand{
     #[command(name="csv",about="Show CSV,or convert CSV to other formats")]
     Csv(CsvOpts),
+    #[command(name="genpass",about="Generate a random password")]
+    GenPass(GenPassOptos),
+
 }
 
 #[derive(Debug,Clone,Copy)]
@@ -39,6 +42,29 @@ pub struct CsvOpts{
     #[arg(long,default_value_t=true)]
     pub header:bool,
 }
+
+#[derive(Debug,Parser)]
+pub struct GenPassOptos{
+
+    #[arg(short,long,default_value_t=16)]
+    pub length:u8,
+
+    #[arg(long,default_value_t=true)]
+    pub uppercase:bool,
+
+    #[arg(long,default_value_t=true)]
+    pub lowercase:bool,
+
+    #[arg(long,default_value_t=true)]
+    pub number:bool,
+
+    #[arg(long,default_value_t=true)]
+    pub symbol:bool,
+
+
+}
+
+
 
 fn verify_input_file(filename:&str) -> Result<String,anyhow::Error>{
     if Path::new(filename).exists()
