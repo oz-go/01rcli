9b685de9de87957cf55975b81cef94e3c4674dab
JxtH <914151610@qq.com>
2024-10-21 13:39:37 +0800

    support generic csv to json and yaml output!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+// rcli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+use rcli::{process_csv,Opts,SubCommand};
+
+
+
+
+
+
+
+fn main() -> anyhow::Result<()> {
+    let opts=Opts::parse();
+
+    match opts.cmd{
+        SubCommand::Csv(opts) => {
+            let output = if let Some(output) = opts.output {
+
+                output.clone()
+            }else{
+                format!("output.{}",opts.format)
+            };
+            process_csv(&opts.input, output,opts.format)?
+        }
+    }
+    // println!("{:?}",opts);
+    Ok(())
 }
+
---
+++ src/opts.rs
@@ -0,0 +1,84 @@
+use clap::Parser;
+use std::{path::Path,fmt,str::FromStr};
+
+#[derive(Debug,Parser)]
+#[command(name = "rcli", version,author,about,long_about=None)]
+pub struct Opts{
+    #[command(subcommand)]
+    pub cmd:  SubCommand,
+}
+
+#[derive(Debug,Parser)]
+pub enum SubCommand{
+    #[command(name="csv",about="Show CSV,or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug,Clone,Copy)]
+pub enum OutputFormat{
+    Json,
+    Yaml,
+}
+
+
+#[derive(Debug,Parser)]
+pub struct CsvOpts{
+    #[arg(short,long,value_parser=verify_input_file)]
+    pub input:String,
+
+    #[arg(short,long)]
+    pub output:Option<String>,
+
+    #[arg(long,value_parser=parser_format, default_value = "json")]
+    pub format:OutputFormat,
+
+
+    #[arg(short,long,default_value_t = ',')]
+    pub delimiter:char,
+
+    #[arg(long,default_value_t=true)]
+    pub header:bool,
+}
+
+fn verify_input_file(filename:&str) -> Result<String,anyhow::Error>{
+    if Path::new(filename).exists()
+{
+    Ok(filename.into())
+}else{ 
+    Err(anyhow::anyhow!("File does not exist"))
+}}
+
+fn parser_format(format:&str) -> Result<OutputFormat,anyhow::Error>{
+    format.parse()
+}
+
+impl From<OutputFormat> for &'static str{
+    fn from(format:OutputFormat)->Self{
+        match format{
+            OutputFormat::Json =>"json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat{
+    type Err = anyhow::Error;
+
+    fn from_str(s:&str) -> Result<Self,Self::Err>{
+        match s{
+        "json" => Ok(OutputFormat::Json),
+        "yaml" => Ok(OutputFormat::Yaml),
+        _ => Err(anyhow::anyhow!("Invalid format")),
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"{}",Into::<&str>::into(*self))
+    }
+}
+
+
+
+
